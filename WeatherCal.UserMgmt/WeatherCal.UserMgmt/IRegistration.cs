578f6b8273f816631f5e29e8abd60edfa554dec0
Maik Schönfeld <schoenfeldmaik@googlemail.com>
2018-03-24 15:56:00 +0100

    added registration interface

---
+++ WeatherCal.UserMgmt/WeatherCal.UserMgmt/IRegistration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherCal.UserMgmt.Entities;
+
+namespace WeatherCal.UserMgmt
+{
+    public interface IRegistration
+    {
+        Task<Feed> AddSubscriptionToFeed(Subscription subscription, Guid? feedGuid);
+        void DeleteSubscription(Guid subscriptionGuid);
+        void DeleteSubscription(Subscription subscription);
+        List<Feed> GetFeeds();
+        List<Subscription> GetSubscriptions(Guid feedGuid);
+        List<Subscription> GetSubscriptions(Feed feed);
+        List<Subscription> GetSubscriptions();
+    }
+}
