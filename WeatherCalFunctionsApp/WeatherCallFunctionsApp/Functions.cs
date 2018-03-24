1f8cf50a758c1f62b2a352cc49845ea1433d3a3c
Dirk Bollmeyer <dirk@bollmeyer.net>
2018-03-24 16:59:49 +0100

    add webjobs app

---
+++ WeatherCalFunctionsApp/WeatherCallFunctionsApp/Functions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.ServiceBus.Messaging;
+using System.IO;
+
+namespace WeatherCallFunctionsApp
+{
+    public class Functions
+    {
+        private static readonly log4net.ILog AIlog =
+            log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        // HourlyWeatherTopicName subscription listener
+        public static void WeatherAppTopicListener(
+            [ServiceBusTrigger("%HourlyWeatherTopicName%", "%WeatherCalSubscriptionName%")] BrokeredMessage message, TextWriter log)
+        {
+            //log.WriteLine("topic received");
+            //var hourlyWeatherDto = message.GetBody<HourlyWeatherDto>();
+            var hourlyWeatherDto = message.GetBody<HourlyWeatherMessage>();
+            log.WriteLine("message received");
+            log.WriteLine("received: {0}", hourlyWeatherDto);
+
+            //var hourlyWeather = JsonConvert.SerializeObject(hourlyWeatherDto);
+        }
+    }
+}
---
+++ WeatherCalFunctionsApp/WeatherCallFunctionsApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.ServiceBus;
+using Microsoft.ServiceBus;
+using System.Configuration;
+
+namespace WeatherCallFunctionsApp
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see https://go.microsoft.com/fwlink/?LinkID=320976
+    class Program
+    {
+        private static string _servicesBusConnectionString;
+        private static NamespaceManager _namespaceManager;
+
+        public static void Main()
+        {
+            _servicesBusConnectionString = AmbientConnectionStringProvider.Instance.GetConnectionString(ConnectionStringNames.ServiceBus);
+            _namespaceManager = NamespaceManager.CreateFromConnectionString(_servicesBusConnectionString);
+            CreateTopicSubscription();
+
+            JobHostConfiguration config = new JobHostConfiguration();
+            ServiceBusConfiguration serviceBusConfig = new ServiceBusConfiguration
+            {
+                ConnectionString = _servicesBusConnectionString
+            };
+            config.UseServiceBus(serviceBusConfig);
+            config.NameResolver = new QueueNameResolver();
+ 
+            JobHost host = new JobHost(config);
+            host.RunAndBlock();
+        }
+
+        private static void CreateTopicSubscription()
+        {
+            var topicName = ConfigurationManager.AppSettings["HourlyWeatherTopicName"];
+            var subcriptionName = ConfigurationManager.AppSettings["WeatherCalSubscriptionName"];
+
+            if (!_namespaceManager.TopicExists(topicName))
+            {
+                _namespaceManager.CreateTopic(topicName);
+            }
+
+            if (!_namespaceManager.SubscriptionExists(topicName, subcriptionName))
+            {
+                _namespaceManager.CreateSubscription(topicName, subcriptionName);
+            }
+       }
+    }
+}
