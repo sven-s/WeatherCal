c2899805d73d93a2fc5029b06996c7624bdfe770
Maik Schönfeld <schoenfeldmaik@googlemail.com>
2018-03-24 13:53:47 +0100

    removed POI dto

--- WeatherCal.UserMgmt/WeatherCal.UserMgmt/Entities/Subscription.cs
+++ WeatherCal.UserMgmt/WeatherCal.UserMgmt/Entities/Subscription.cs
@@ -7,12 +7,18 @@ namespace WeatherCal.UserMgmt.Entities
     public class Subscription
     {
         public Guid Id { get; set; } = Guid.NewGuid();
-        public PointOfInterest PointOfInterest { get; set; }
 
         public double WindSpeedMin { get; set; }
         public double WindSpeedMax { get; set; }
 
         public List<(int minAngle, int maxAngle)> WindBearings { get; set; }
+
+        //POI
+
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
 
     }
 }
