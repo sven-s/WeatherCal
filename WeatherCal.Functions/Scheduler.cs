47d8fd747e10c515a3890915c0f8de720f6b0eae
Sven Soennichsen <sven@sven-s.de>
2018-03-24 16:32:04 +0100

    retrieve sample data from darksky

--- WeatherCal.Functions/Scheduler.cs
+++ WeatherCal.Functions/Scheduler.cs
@@ -21,22 +21,33 @@ namespace WeatherCal.Functions
         [FunctionName("Scheduler")]
         public static void Run([TimerTrigger("0 */30 * * * *")]TimerInfo myTimer, TraceWriter log)
         {
-
+            log.Info($"Starting scheduler at: {DateTime.Now}");
             // get the locations from the table storage
             var locations = new List<Location>();
 
             // for all locations query the weather api
-            var serviceTokenProvider = new AzureServiceTokenProvider();
-            var keyVault = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(serviceTokenProvider.KeyVaultTokenCallback));
-            var secretUri = SecretUri("Darksky-Key");
-            var key = keyVault.GetSecretAsync(secretUri).GetAwaiter().GetResult();
-            log.Info(key.Value);
-            var client = new RestClient($"https://api.darksky.net/forecast/{key.Value}/37.8267,-122.4233");
 
+            try
+            {
+                var serviceTokenProvider = new AzureServiceTokenProvider();
+                var keyVault = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(serviceTokenProvider.KeyVaultTokenCallback));
+                var secretUri = SecretUri("Darksky-Key");
+                var key = keyVault.GetSecretAsync(secretUri).GetAwaiter().GetResult();
+                var client = new RestClient("https://api.darksky.net");
+
+                var request = new RestRequest($"forecast/{key.Value}/37.8267,-122.4233", Method.GET);
+                IRestResponse response = client.Execute(request);
+                var content = response.Content; // raw content as string
+                log.Info(content);
+
+            }
+            catch (Exception ex)
+            {
+                log.Error("Cannot retrieve key", ex);
+                throw;
+            }
 
             // write the response into a table storage
-
-
 
 
             log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
@@ -44,7 +55,7 @@ namespace WeatherCal.Functions
 
         public static string SecretUri(string secret)
         {
-            return $"{WebConfigurationManager.AppSettings["KeyValutUri"]}/Secrects/{secret}";
+            return $"{WebConfigurationManager.AppSettings["KeyVaultUri"]}Secrets/{secret}";
         }
 
     }
