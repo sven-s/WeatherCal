0d93f2e24574633751f14aad29363eb15eea8199
Dirk Bollmeyer <dirk@bollmeyer.net>
2018-03-25 12:35:52 +0200

    add ref to dto lib

--- WeatherCalFunctionsApp/WeatherCallFunctionsApp/Functions.cs
+++ WeatherCalFunctionsApp/WeatherCallFunctionsApp/Functions.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.Azure.WebJobs;
+﻿using System;
+using Microsoft.Azure.WebJobs;
 using Microsoft.ServiceBus.Messaging;
 using System.IO;
+using Ical.Net;
+using WeatherCal.Contracts;
+//using Ical.Net.DataTypes;
 
 namespace WeatherCallFunctionsApp
 {
@@ -16,6 +20,23 @@ namespace WeatherCallFunctionsApp
             log.WriteLine("topic received v1.1");
             var hourlyWeatherDto = message.GetBody<HourlyWeatherMessage>();
             log.WriteLine("received: {0}", hourlyWeatherDto.Guid);
+
+
+
+            var calendar = new Calendar();
+            calendar.AddProperty("X-WR-CALNAME", "Azure Functions Webinar"); // sets the calendar title
+            calendar.AddProperty("X-ORIGINAL-URL", "http://aka.ms/AzureFunctionsLive");
+            calendar.AddProperty("METHOD", "PUBLISH");
+
+            //var icalevent = new Event()
+            //{
+            //    DtStart = new CalDateTime(new DateTime(2017, 07, 06, 18, 0, 0, DateTimeKind.Utc)),
+            //    DtEnd = new CalDateTime(new DateTime(2017, 07, 06, 19, 0, 0, DateTimeKind.Utc)),
+            //    Created = new CalDateTime(DateTime.Now),
+            //    Location = "http://aka.ms/AzureFunctionsLive",
+            //    Summary = "Azure Function Webinar",
+            //    Url = new Uri("http://aka.ms/AzureFunctionsLive")
+            //};
 
             //var hourlyWeather = JsonConvert.SerializeObject(hourlyWeatherDto);
         }
